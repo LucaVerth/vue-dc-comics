4459494a5fe13a86d46d8fed18c9cad10f67ace5
LucaVerth <luca.verthuy@gmail.com>
2021-11-24 16:28:46 +0100

    App init with all blocks

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <div id="app">
-    
+    <Header/>
+    <Main/>
+    <Section/>
+    <Footer/>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Section from './components/Section.vue'
+import Footer from './components/Footer.vue'
 
 
 export default {
   name: 'App',
   components: {
-    
+    Header,
+    Main,
+    Section,
+    Footer
   }
 }
 </script>
