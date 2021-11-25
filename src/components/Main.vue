a7b82156e6881f404d4fd71c520f8e80b8c9a831
LucaVerth <luca.verthuy@gmail.com>
2021-11-25 17:47:31 +0100

    Button added between main and jumbo

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,6 +3,7 @@
     <Jumbotron/>
     <div class="main-container">
       <div class="card-list">
+        <div class="custom-btn-main">Current Series</div>
         <Comics
           v-for="(product, index) in ProductsItems"
           :key="index"
@@ -42,6 +43,7 @@ main{
   background-color: #000;
   color: #fff;
   .main-container{
+    position: relative;
     @include container();
     @include align();
     min-height: 100px;
@@ -57,6 +59,18 @@ main{
       background-color: #0282F9;
     }
   }
+  .custom-btn-main{
+    position: absolute;
+    top: -20px;
+    left: 50px;
+    @include button();
+    width: 200px;
+    height: 40px;
+    background-color: #0282F9;
+    font-weight: bold;
+    font-size: 1.2rem;
+  }
+  
 }
 .card-list {
   width: 100%;
