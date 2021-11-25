b8512b40aba07cf1d0d0b8a45ab9a48c05d9a207
LucaVerth <luca.verthuy@gmail.com>
2021-11-25 12:16:27 +0100

    Static Comics added // working on v-for directive to print

--- src/components/ComicsCard.vue
+++ src/components/ComicsCard.vue
@@ -1,6 +1,53 @@
 <template>
-  <div class="card">
-    
+  <div class="card-list">
+    <div class="card">
+      <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+    <h4>Action Comics</h4>
+    </div>
+    <div class="card">
+      <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+    <h4>Action Comics</h4>
+    </div>
+    <div class="card">
+      <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+    <h4>Action Comics</h4>
+    </div>
+    <div class="card">
+      <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+    <h4>Action Comics</h4>
+    </div>
+    <div class="card">
+      <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+    <h4>Action Comics</h4>
+    </div>
+    <div class="card">
+      <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+    <h4>Action Comics</h4>
+    </div>
+    <div class="card">
+      <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+    <h4>Action Comics</h4>
+    </div>
+    <div class="card">
+      <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+    <h4>Action Comics</h4>
+    </div>
+    <div class="card">
+      <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+    <h4>Action Comics</h4>
+    </div>
+    <div class="card">
+      <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+    <h4>Action Comics</h4>
+    </div>
+    <div class="card">
+      <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+    <h4>Action Comics</h4>
+    </div>
+    <div class="card">
+      <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+    <h4>Action Comics</h4>
+    </div>
   </div>
 </template>
 
@@ -11,5 +58,14 @@ export default {
 </script>
 
 <style lang="scss">
-
+.card-list {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+.card{
+  flex-basis: calc((100% / 6));
+  margin: 40px 0;
+  padding: 0 20px;
+}
 </style>
